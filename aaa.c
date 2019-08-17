a1e2351c72d64b487f531489eaeab3794ec6f43a
Ling Cheng Nan <nan800130@gmail.com>
2019-08-17 14:58:33 +0800

    pushed from DELab-Sam Desktop

--- aaa.c
+++ aaa.c
@@ -15,3 +15,5 @@ int main(){
 void sayhello(){
     printf("Hello World!\n");
 }
+
+
---
+++ bbb.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    printf("Test for DELab-Sam Desktop");
+    return 0;
+}
