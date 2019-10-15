556adfc8db39521809105121e45197549ee5820d
Ling Cheng Nan <nan800130@gmail.com>
2019-10-15 10:12:46 +0800

    add skyline.c

---
+++ skyline.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Coordinate{
+   
+   /dimension/
+   double x;
+   double y;
+   double z;
+   
+};
+
+/*prototype declaration*/
+
+int main(void){
+    
+    /*variables declaration and initialization*/
+    int NumberOfNodes;
+    
+    Coordinate Coordinates[NumberOfNodes];
+
+    /*generate random number of nodes*/
+    
+    return 0;
+}
+
+/*function implmentation*/
+int generateRandomNodes(){
+
+}
+
+/*Skyline Algorithm*/
+void ConventoinalSkyline(Coordinate *Coordinates[]){
+    
+}
