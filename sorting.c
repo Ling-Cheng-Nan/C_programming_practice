48741f504afc657d6f7aa8d0aed98f8cdde8e222
Ling Cheng Nan <nan800130@gmail.com>
2019-12-06 16:01:47 +0800

    add sorting.c

---
+++ sorting.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+
+#define SIZE 50
+#define max 1000
+#define min 1
+
+void assignArray(int[], int);
+void showArrayContent(int[], int);
+void bubbleSort(int[], int);
+void insertionSort(int[], int);
+
+int main(void){
+	
+	srand(time(NULL));
+	
+	//initialize array
+	int unsortedArray[SIZE];
+	
+	assignArray(unsortedArray, SIZE);	
+	
+	//before sortinog
+	printf("\nbefore sorting array....\n");
+	showArrayContent(unsortedArray, SIZE);
+	
+	/* seveeral sorting functions below
+	 * 1. bubble sorting
+	 * 2. insertion sorting
+	 * 3. 
+	 * */
+	
+	//bubbleSort(unsortedArray, SIZE);
+	insertionSort(unsortedArray , SIZE);
+	//after sorting
+	printf("\nafter sorting array.....\n");
+	showArrayContent(unsortedArray, SIZE);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
+void assignArray(int a[],int size){
+	
+	//int max = 100;
+	//int min = 0;
+
+	for(int i = 0 ; i < size ; i++){
+		a[i] = (rand()%(max - min)) + min;
+	}
+}
+
+void showArrayContent(int a[],int size){
+	
+	for(int i = 0 ; i < size ; i++){
+		(( i%10) == 0 && (i!=0) )? printf("\n%d ", a[i]) : printf("%d ", a[i]);
+	}
+
+}
+
+void swap(int* a, int* b){
+	
+}
+
+void bubbleSort(int a[], int size){
+	int temp;
+	int passes = 0;
+	int comparisons = 0;
+
+	for(int i = 0 ; i < size-1 ; i++){
+		
+		for(int j = (i+1) ; j < size ; j++){
+			
+			//sorting by acsending order
+			if(a[i] >= a[j]){
+				//swap two elements
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+			
+			//increase number of comparisons
+			comparisons++;
+		}
+		
+		//increase number of passes 
+		passes++;
+	}
+
+	printf("\n\nthere are %d comparisons during bubbleSort.\n", comparisons);
+	printf("there are %d passes during bubbleSort.\n", passes);
+
+}
+
+void insertionSort(int a[], int size){
+	
+	int temp;
+	
+	for(int i = 0 ; i < size ; i++){
+		
+		if(i==0){ 
+			continue;
+		}
+		else{
+			for(int j = 0 ; j < (i-1) ; j++){
+				if(a[i] <= a[j]){
+					//swap two elements
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}else continue;	
+			}
+		}
+	}
+}
+
+
