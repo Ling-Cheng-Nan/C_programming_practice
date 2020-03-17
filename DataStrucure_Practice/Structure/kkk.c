c7ed83a1c4a2c8260360829ce38d4c8d63178b6d
Ling-Cheng-Nan <nan800130@gmail.com>
2020-03-17 22:49:03 +0800

    lll

--- DataStrucure_Practice/Structure/kkk.c
+++ DataStrucure_Practice/Structure/kkk.c
@@ -1,21 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct date{
-    int day;
-    int month;
-    int year;
-} Date;
+#include <string.h>
+
+#include "type.h"
+
+void printDate(Date );
+void printDatePointer(Date *);
 
 int main(void){
-    struct date normalDate = {15, 07, 2032};
-    Date borroweDate = {31, 12, 2012};
+    
+    struct date normalDate = {15, "Aug", 2032};
+    Date borroweDate = {31, "Dec", 2012};
     Date* pD = &normalDate;
     Date* pB = &borroweDate;
 
-    //scanf("%d %d %d", pD->day, pD->month, pD->year);
+    // scanf("%d %d %d", &normalDate.month, &normalDate.month, &normalDate.year);
 
-    printf("normal date : %d %d, %d\n", pD->day, pD->month, pD->year);
-    printf("borrow date : %d %d, %d\n", pB->day, pB->month, pB->year);
+    printf("normal date : %d %s, %d\n", pD->day, pD->month, pD->year);
+    printf("borrow date : %d %s, %d\n", pB->day, pB->month, pB->year);
     
+    printDate(normalDate);
+    printDate(*pD);
+    printDatePointer(pD);
+    
+
+    /*************************/
+    Student pupil[100];
+    // FILE * in = fopen("input.txt", "r");
+    // char temp[31];
+    // getString(in, temp);
+    // for(int i = 0 ; strcmp(temp, "END") ; i++){
+        
+    //     strcpy(pupil[i].name, temp);
+    //     fscanf(in, "%d", &pupil[i].age);
+    //     pupil[i].gender = readChar(in);
+
+    //     getString(in, temp);
+    // }
+    
+    // for(int i=0;i<100;i++)
+    //     printf("%s %d %c\n",pupil[i].name, pupil[i].age, pupil[i].gender);
+
     return 0;
+}
+
+void printDate(Date d){
+    printf("%s %d, %d\n", d.month, d.day, d.year);
+}
+
+void printDatePointer(Date *d){
+    printf("%s %d, %d\n", d->month, d->day, d->year);
+}
+
+void search(){
+    
 }
---
+++ DataStrucure_Practice/Structure/type.h
@@ -0,0 +1,13 @@
+typedef struct date{
+    int day;
+    char month[4]; //one position for \0 to end string
+    //int month;
+    int year;
+} Date;
+
+typedef struct student{
+    char name[31];
+    int age;
+    char gender;
+} Student;
+
