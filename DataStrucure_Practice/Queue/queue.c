6cd0da42ebe6446b90941b07b2bb2836fb4a4488
Ling-Cheng-Nan <nan800130@gmail.com>
2020-04-17 00:14:38 +0800

    queue

--- DataStrucure_Practice/Queue/queue.c
+++ DataStrucure_Practice/Queue/queue.c
@@ -20,14 +20,16 @@ struct queue{
 };
 
 /* function prototype declaration */
-int ask_for_options();
-int isEmpty(Queue* );
+int  ask_for_options();
 void init_queue(Queue* );
 void enqueue(Queue* , int);
-int dequeue(Queue* );
+int  dequeue(Queue* );
+int  isEmpty(Queue* );
+void display(Queue* );
 
 /* main */
 int main(){
+    /* variables initialization*/
     int option, value;
     int extract_value;
     Queue queue;
@@ -42,21 +44,28 @@ int main(){
                 /* enqueue */
                 printf("Enter the value you want to push into the queue : \n");
                 scanf("%d", &value);
-
+                enqueue(ptr, value);
+                display(ptr);
                 break;
+
             case 2:
                 /* dequeue */
-
+                extract_value = dequeue(ptr);
+                printf("value of element extracted from front of queue : %d\n", extract_value);
+                display(ptr);
                 break;
+
             case 3:
                 /* show elements in queue */
-
+                display(ptr);
                 break;
+
             case 4:
                 /* exit */
                 printf("You are gonna leave this program.\n");
                 exit(0);
-                break;    
+                break;
+
             default:
                 printf("Error option, try again.\n");
                 break;
@@ -97,29 +106,42 @@ void enqueue(Queue* qu, int value){
         
         /*if queue is empty*/
         if(qu->size == 0){
+            temp->prev = NULL;
             qu->front = temp; /*front and rear is indicate to same element*/
+            
         }
 
         temp->data = value;
         temp->next = NULL;
-        temp->prev = qu->rear; //move last element pointer to new node
-        
-        qu->size ++; //increase queue size
+        temp->prev = qu->rear; 
+        qu->rear = temp;//move last element pointer to new node
+        (qu->size)++; //increase queue size
+        printf("the size of queue is : %d.\n", qu->size);
     }
-
-
 }
 
 int dequeue(Queue* qu){
 
-    int extract_value = 0;
+    int extract_value = -1;
     
+    /*check the size of queue is empty or not*/
     if(isEmpty(qu)){
         printf("the Queue is empty, can not dequeue anymore.\n");
     }else{
-        /**/
+        Node* temp = qu->front;
+        if(qu->size == 1){
+            qu->rear = (qu->rear)->next;
+        }
+        extract_value = temp->data;
+        qu->front = (qu->front)->next;
+        
+        free(temp);
+        
+        qu->size --; //decrease queue size
+        printf("the size of queue is : %d.\n", qu->size);
     }
-
+    
+    return extract_value;
 }
 
 int isEmpty(Queue* qu){
@@ -127,4 +149,25 @@ int isEmpty(Queue* qu){
         return 1;
     else 
         return 0;
+}
+
+void display(Queue* qu){
+
+    Node* temp = qu->front;
+
+    if(isEmpty(qu)) 
+        printf("THE QUEUE IS EMPTY\n");
+    else{
+        // if(temp->prev == NULL){
+        //         printf("(HEAD OF QUEUE)| ");
+        // }
+        while(temp != NULL){
+            printf("| %d | ", temp->data);
+            temp = temp->next;
+        }
+        
+        // if(temp->next == NULL){
+        //         printf(" |(END OF QUEUE)\n");
+        // }
+    }
 }
